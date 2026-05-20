2b55bbe40cc1bc6429f53c4a02366bf2987e16b2
callmelovevsick <callmelovevsick@proton.me>
2026-05-21 00:30:32 +0700

    add smt else

--- main.cpp
+++ main.cpp
@@ -1,35 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int celsius(string s) {
-    int ans = 0;
-    for (int i = 0; i < (int)s.size(); i++) {
-        if (s[i] >= '0' && s[i] <= '9') {
-            ans = ans * 10 + (s[i] - '0');
-        }
-    }
-    return fixed << setprecision(2) << ans * 9 / 5 + 32;
-}
-int fahrenheit(string s) {
-    int ans = 0;
-    for (int i = 0; i < (int)s.size(); i++) {
-        if (s[i] >= '0' && s[i] <= '9') {
-            ans = ans * 10 + (s[i] - '0');
-        }
-    }
-    return fixed << setprecision(2) << (ans - 32) * 5 / 9;
+#define fast() ios::sync_with_stdio(false); cin.tie(0);
+#define in(file) freopen(file ".inp", "r", stdin);
+#define out(file) freopen(file ".out", "w", stdout);
+
+int nhi_phan(int n) {
+    if (n == 0) return 0;
+    return (n % 2) + 10 * nhi_phan(n / 2);
 }
 
-int32_t main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    string s; cin >> s;
-    for (int i = 0; i < (int)s.size(); i++) {
-        if (s[i] == 'C') {
-            cout << fixed << setprecision(2) << celsius(s);
-            return 0;
-        }
-    }
-    cout << fixed << setprecision(2) << fahrenheit(s);
+int main() {
+    fast();
+    
+    int n; cin >> n;
+    cout << nhi_phan(n);
+    
     return 0;
 }
