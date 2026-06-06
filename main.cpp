3447889b12a00810c1c35a21edec38f423591260
callmelovevsick <callmelovevsick@proton.me>
2026-06-06 22:31:24 +0700

    idk

--- main.cpp
+++ main.cpp
@@ -5,24 +5,24 @@ using namespace std;
 #define in(file) freopen(file ".inp", "r", stdin);
 #define out(file) freopen(file ".out", "w", stdout);
 
-inline long long modpow(long long a, long long b, long long mod) {
-    long long res = 1;
-    a %= mod;
-
-    while (b) {
-        if (b & 1)
-            res = (__int128)res * a % mod;
-
-        a = (__int128)a * a % mod;
-        b >>= 1;
-    }
-
-    return res;
-}
-
 int main() {
     fast();
-    cout << modpow(5, 9);
+
+    in("main");
+    out("main");
+    
+    int n, q; cin >> n >> q;
+    vector<int> a(n+1);
+    int ans = 0;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    while (q--) {
+        int l, r, x; cin >> l >> r >> x;
+        for (int i = l; i <= r; i++) {
+            if (a[i] == x) ans++;
+        }
+        cout << ans << endl;
+        ans = 0;
+    }
     
     return 0;
 }
