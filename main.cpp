7e4a5a0552cd2f730136761322d0c94a53744b0c
callmelovevsick <callmelovevsick@proton.me>
2026-06-16 19:58:48 +0700

    fixed_git

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 #define fast() ios::sync_with_stdio(false); cin.tie(0);
 #define in(file) freopen(file ".inp", "r", stdin);
 #define out(file) freopen(file ".out", "w", stdout);
 
-const int MAXN = 10000+5;
-int a[MAXN];
-
 int main() {
     fast();
-    
+    in("TEAM");
+    out("TEAM");
+
     int n, k; cin >> n >> k;
-    for (int i = 0; i < n; i++) cin >> a[i];
-    k %= n; 
-    for (int i = n-k; i < n; i++) cout << a[i] << " ";
-    for (int i = 0; i < n-k; i++) { 
-        cout << a[i]; 
-        if (i < n-k-1) cout << " "; 
+    vector<long long> count(k, 0);
+    long long ans = 0;
+    for (int i = 0; i < n; ++i) {
+        long long a;
+        cin >> a;
+        int r = a % k;
+        int target = (k - r) % k;
+        ans += count[target];
+        count[r]++;
     }
-    cout << "\n";
+    cout << ans << "\n";
     return 0;
 }
