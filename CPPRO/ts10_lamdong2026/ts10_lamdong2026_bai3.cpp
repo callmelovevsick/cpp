d582b7770e304d0bd2ae992e6998bdc652227e4a
callmelovevsick <callmelovevsick@proton.me>
2026-07-11 08:47:02 +0700

    ts10_lamdong2026_bai3

--- CPPRO/ts10_lamdong2026/ts10_lamdong2026_bai3.cpp
+++ CPPRO/ts10_lamdong2026/ts10_lamdong2026_bai3.cpp
@@ -13,7 +13,7 @@ bool prime(int n) {
     if (n <= 3) return true;
     if (n % 2 == 0 || n % 3 == 0) return false;
     for (int i = 5; i*i <= n; i += 6) {
-        if (n % i == 0 || n % (i+1) == 0) return false;
+        if (n % i == 0 || n % (i+2) == 0) return false;
     }
     return true;
 }
